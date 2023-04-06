a69f3591e6fd819b3200521b10a92a38d825e360
Timur <timur.hike@gmail.com>
2023-04-06 13:40:18 +0300

    Add serializers

---
+++ chat/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from chat.models import Thread, Message
+
+
+class ThreadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Thread
+        fields = ("id", "participants", "created", "updated")
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ("id", "sender", "text", "thread", "created", "is_read")
