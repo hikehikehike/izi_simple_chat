08b7af472b34c636935c7ad27439cdfd94e43b47
Timur <timur.hike@gmail.com>
2023-04-06 20:37:35 +0300

    Add validation

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from chat.models import Thread, Message
@@ -6,10 +7,37 @@ from chat.models import Thread, Message
 class ThreadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Thread
-        fields = ("id", "participants", "created", "updated",)
+        fields = (
+            "id",
+            "participants",
+            "created",
+            "updated",
+        )
+        read_only_fields = (
+            "created",
+            "updated",
+        )
+
+    def validate(self, data):
+        if len(data["participants"]) != 2:
+            raise ValidationError("Thread must have exactly 2 participants.")
+        return data
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Message
-        fields = ("id", "sender", "text", "thread", "created", "is_read",)
+        fields = (
+            "id",
+            "sender",
+            "text",
+            "thread",
+            "created",
+            "is_read",
+        )
+
+    def create(self, validated_data):
+        validated_data["sender"] = self.context["request"].user
+        return super().create(validated_data)
