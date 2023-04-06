018b6ebf98dc35e320e3a6571cb39eceb6b64411
Timur <timur.hike@gmail.com>
2023-04-06 13:22:33 +0300

    Add models Thread and Message

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Thread(models.Model):
+    participants = models.ManyToManyField(User, related_name="threads")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-updated"]
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
+    text = models.TextField()
+    thread = models.ForeignKey(
+        Thread, on_delete=models.CASCADE, related_name="messages"
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created"]
