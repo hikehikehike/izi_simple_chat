b214dd0b28d8d80b106f220a389050fcf0b9552b
Timur <timur.hike@gmail.com>
2023-04-06 22:13:05 +0300

    Add new serializers in views. Add "user can see message only if he has in thread"

--- chat/views.py
+++ chat/views.py
@@ -1,14 +1,33 @@
 from rest_framework import viewsets
 
 from chat.models import Thread, Message
-from chat.serializers import ThreadSerializer, MessageSerializer
+from chat.serializers import (
+    ThreadSerializer,
+    MessageSerializer,
+    MessageListSerializer,
+    ThreadListSerializer,
+)
 
 
 class ThreadViewSet(viewsets.ModelViewSet):
     queryset = Thread.objects.all()
     serializer_class = ThreadSerializer
 
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ThreadListSerializer
+        return ThreadSerializer
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MessageListSerializer
+        return MessageSerializer
+
+    def get_queryset(self):
+        thread = Thread.objects.filter(participants=self.request.user)
+        return Message.objects.filter(thread__in=thread)
